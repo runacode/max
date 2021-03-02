a0887cffe85a96ebce5c4767f43b4bff54fbee97
runacode <59107019+runacode@users.noreply.github.com>
2021-03-02 17:02:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php $company = "T AND M LANDSCAPING";
-$phone = "(719) 948-1559";
-	$address1 = "563 S PUEBLO BLVD";
-	$address2 = "PUEBLO, CO 81005";
+<?php $company = "MAX POWER AUTOMOTIVE LLC";
+$phone = "(661) 379-6875";
+	$address1 = "324 DONNA AVE";
+	$address2 = "BAKERSFIELD, CA 93304-2340";
 ?>
 <!DOCTYPE html>
 <html style="font-size: 16px;">
@@ -73,8 +73,8 @@ $phone = "(719) 948-1559";
                     
                   </div>
                 </div>
-                <h3 class="u-text u-text-7">Contact us</h3>
-                <h6 class="u-text u-text-8">Do not hesitate to start a new project with us!</h6>
+                <h3 class="u-text u-text-7">Get In Touch!</h3>
+                <h6 class="u-text u-text-8">Let's work together!</h6>
               </div>
             </div>
           </div>
